afd6085ac6d89089be5e85106948664306d06a5c
zhipengzou <zhipengzou@caijing.com.cn>
2018-01-20 21:32:31 +0800

    fillable

--- app/Models/ShuoshuoComment.php
+++ app/Models/ShuoshuoComment.php
@@ -6,15 +6,26 @@ class ShuoshuoComment extends BaseModel
 {
     protected $guarded = [];
 
-    public function user() {
+    protected $fillable = [
+        'parent_id',
+        'commentuser_id',
+        'user_id',
+        'shuoshuo_id',
+        'content'
+    ];
+
+    public function user()
+    {
         return $this->belongsTo('App\Models\User');
     }
 
-    public function parent() {
+    public function parent()
+    {
         return $this->belongsTo('App\Models\ShuoshuoComment', 'parent_id');
     }
 
-    public function shuoshuo() {
+    public function shuoshuo()
+    {
         return $this->belongsTo('App\Models\Shuoshuo');
     }
 }
